6d6f20dba9123b1bf9589906f69f7a42a6cef625
David McClure <davidwilliammcclure@gmail.com>
2014-09-10 14:29:56 -0700

    Wraps the OSD instance up in a view, sketches in a router.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,28 +2,63 @@
 
 require('osd');
 var config = require('config');
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
 
 
-// Spin up OSD.
-var viewer = OpenSeadragon({
+var NetworkView = Backbone.View.extend({
 
   id: 'text',
 
-  immediateRender: true,
-  showNavigationControl: false,
-  showNavigator: true,
+  /**
+   * Initialize Openseadragon.
+   */
+  initialize: function() {
 
-  tileSources: {
-    Image: {
-      xmlns: 'http://schemas.microsoft.com/deepzoom/2008',
-      Url: config.bucket+'/'+config.slug+'/tiles/',
-      Format: 'jpg',
-      TileSize: 512,
-      Size: {
-        Height: 10000,
-        Width: 10000
+    this.viewer = OpenSeadragon({
+
+      id: 'text',
+
+      immediateRender: true,
+      showNavigationControl: false,
+      showNavigator: true,
+
+      tileSources: {
+        Image: {
+          xmlns: 'http://schemas.microsoft.com/deepzoom/2008',
+          Url: config.bucket+'/'+config.slug+'/tiles/',
+          Format: 'jpg',
+          TileSize: 512,
+          Size: {
+            Height: 10000,
+            Width: 10000
+          }
+        }
       }
-    }
+
+    });
+
+  }
+
+});
+
+
+var Router = Backbone.Router.extend({
+
+  routes: {
+    ':x/:y/:z': 'focus'
+  },
+
+  /**
+   * Focus on a specific location.
+   *
+   * @param {String} x
+   * @param {String} y
+   * @param {String} z
+   */
+  focus: function(x, y, z) {
+    console.log(x, y, z);
   }
 
 });
